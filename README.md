603e36c6b47d366df17303f4ff3549714ca5e102
Garry Ing <garry.ing@gmail.com>
2016-02-27 21:50:54 -0500

    clean up field getter

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 
 ## A plugin for OCAD U's Illustration Department by [Garry Ing](http://garrying.com/ "Link to garrying.com")
 
-Currently working on *Wordpress 3.3.1*
+Currently working on *Wordpress 4.4.2*
 
 ## Features
 
 * Illustrator Post Type
 * Grad Year Taxonomies
 * Meta Fields for Illustrator Post Type
+* Illustator Post Type support for WP REST API
 
 ## Site URLs
 
--- ocadu-illustration.php
+++ ocadu-illustration.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: OCAD U Illustration
 Plugin URI: http://www.ocaduillustration.com
-Description: Brings support to WP3 for things like Event and Illustrator post types.
+Description: Brings support to WP4 for things like Event and Illustrator post types.
 Author: Garry Ing
 Version: 1.0
 Author URI: http://garrying.com
@@ -83,75 +83,62 @@ function custom_enter_title( $input ) {
 
 // Set custom meta fields.
 
-add_action("admin_init", "admin_init");
+add_action( "admin_init", "admin_init" );
 
-function admin_init(){
-  add_meta_box("credits_meta", "Illustrator Details", "illustrator_meta", "illustrator", "side", "high");
+function admin_init() {
+  add_meta_box( "credits_meta", "Illustrator Details", "illustrator_meta", "illustrator", "side", "high" );
 }
 
-function illustrator_meta() {
+function illustrator_get_custom_field( $value ) {
   global $post;
-  $custom = get_post_custom($post->ID);
-  if (empty($custom)) {
-    ?>
-    <p><label for="illu_title">Thesis Title:</label><br />
-    <textarea id="illu_title" name="illu_title" style="width:100%"></textarea></p>
-    <p><label for="illu_email">Email Address:</label><br />
-    <input type="email" id="illu_email" name="illu_email" value="" style="width:100%"></p>
-    <p><label for="illu_sites">Website:</label><br />
-    <input type="url" id="illu_sites" name="illu_sites" value="" style="width:100%"><br />
-    <small>Include http://</small></p>
-    <p><label for="illu_sites_2">Website:</label><br />
-    <input type="url" id="illu_sites_2" name="illu_sites_2" value="" style="width:100%"><br />
-    <small>Include http://</small></p>
-    <p><label for="illu_phone">Telephone:</label><br />
-    <input type="tel" id="illu_phone" name="illu_phone" value="" style="width:100%"><br />
-    <small>Example: (416) 123-4567</p>
-    <?php
-  } else {
-    $illu_email = $custom["illu_email"][0];
-    $illu_sites = $custom["illu_sites"][0];
-    $illu_sites_2 = $custom["illu_sites_2"][0];
-    $illu_phone = $custom["illu_phone"][0];
-    $illu_title = $custom["illu_title"][0];
-    ?>
-      <p><label for="illu_title">Thesis Title:</label><br />
-      <textarea id="illu_title" name="illu_title" style="width:100%"><?php echo $illu_title; ?></textarea></p>
-      <p><label for="illu_email">Email Address:</label><br />
-      <input type="email" id="illu_email" name="illu_email" value="<?php echo $illu_email; ?>" style="width:100%"></p>
-      <p><label for="illu_sites">Website:</label><br />
-      <input type="url" id="illu_sites" name="illu_sites" value="<?php echo $illu_sites; ?>" style="width:100%"><br />
-      <small>Include http://</small></p>
-      <p><label for="illu_sites_2">Website:</label><br />
-      <input type="url" id="illu_sites_2" name="illu_sites_2" value="<?php echo $illu_sites_2; ?>" style="width:100%"><br />
-      <small>Include http://</small></p>
-      <p><label for="illu_phone">Telephone:</label><br />
-      <input type="tel" id="illu_phone" name="illu_phone" value="<?php echo $illu_phone; ?>" style="width:100%"><br />
-      <small>Example: (416) 123-4567</p>
-    <?php
-  }
+    $custom_field = get_post_meta( $post->ID, $value, true );
+    if ( !empty( $custom_field ) )
+      return is_array( $custom_field ) ? stripslashes_deep( $custom_field ) : stripslashes( wp_kses_decode_entities( $custom_field ) );
+  return false;
+}
+
+function illustrator_meta( $post ) {
+  ?>
+  <p>
+    <label for="illu_title">Thesis Title</label><br />
+    <textarea id="illu_title" name="illu_title" style="width:100%"><?php echo illustrator_get_custom_field( 'illu_title' ); ?></textarea>
+  </p>
+  <p>
+    <label for="illu_email">Email Address</label><br />
+    <input type="email" id="illu_email" name="illu_email" value="<?php echo illustrator_get_custom_field( 'illu_email' ); ?>" style="width:100%">
+  </p>
+  <p>
+    <label for="illu_sites">Website</label><br />
+    <input type="url" id="illu_sites" name="illu_sites" placeholder="Include http://" value="<?php echo illustrator_get_custom_field( 'illu_sites' ); ?>" style="width:100%">
+  </p>
+  <p>
+    <label for="illu_sites_2">Website</label><br />
+    <input type="url" id="illu_sites_2" name="illu_sites_2" placeholder="Include http://" value="<?php echo illustrator_get_custom_field( 'illu_sites_2' ); ?>" style="width:100%">
+  </p>
+  <p>
+    <label for="illu_phone">Telephone</label><br />
+    <input type="tel" id="illu_phone" name="illu_phone" placeholder="Example: (416) 123-4567" value="<?php echo illustrator_get_custom_field( 'illu_phone' ); ?>" style="width:100%">
+  </p>
+  <?php
 }
 
 // Save meta fields.
 
-function save_details(){
-  global $post;
-  if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-    return $post->ID;
-  }
-  if (isset( $_POST["illu_email"] ))
-    update_post_meta($post->ID, "illu_email", $_POST["illu_email"]);
-  if (isset( $_POST["illu_sites"] ))
-    update_post_meta($post->ID, "illu_sites", $_POST["illu_sites"]);
-  if (isset( $_POST["illu_sites_2"] ))
-    update_post_meta($post->ID, "illu_sites_2", $_POST["illu_sites_2"]);
-  if (isset( $_POST["illu_phone"] ))
-    update_post_meta($post->ID, "illu_phone", $_POST["illu_phone"]);
-  if (isset( $_POST["illu_title"] ))
-    update_post_meta($post->ID, "illu_title", $_POST["illu_title"]);
+function save_details( $post_id ){
+  if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+  if ( isset( $_POST["illu_email"] ) )
+    update_post_meta( $post_id, "illu_email", $_POST["illu_email"] );
+  if ( isset( $_POST["illu_sites"] ) )
+    update_post_meta( $post_id, "illu_sites", $_POST["illu_sites"] );
+  if ( isset( $_POST["illu_sites_2"] ) )
+    update_post_meta( $post_id, "illu_sites_2", $_POST["illu_sites_2"] );
+  if ( isset( $_POST["illu_phone"] ) )
+    update_post_meta( $post_id, "illu_phone", $_POST["illu_phone"] );
+  if ( isset( $_POST["illu_title"] ) )
+    update_post_meta( $post_id, "illu_title", $_POST["illu_title"] );
 }
 
-add_action('save_post', 'save_details');
+add_action( 'save_post', 'save_details' );
 
 // Extending WP-API with querying media based on post parent.
 
